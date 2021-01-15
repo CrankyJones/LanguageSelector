74a7d5595d445e2f9fd321f849e5c06419b9fad7
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-15 10:21:06 -0800

    Get logic working

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
-  $(document).submit(function(event){
+$(document).ready(function () {
+  $("#formCode").submit(function (event) {
     event.preventDefault();
-    $(".Csharp").hide();
-    $(".python").hide();
-    $(".swift").hide();
-    $(".javascript").hide();
-    const animal = $("#animal").val();
+    $("#Csharp").hide();
+    $("#python").hide();
+    $("#swift").hide();
+    $("#javascript").hide();
+    const animal = parseInt($("#animal").val());
     const snakes = $("input:radio[name=snakes]:checked").val();
     const music = $("input:radio[name=music]:checked").val();
-    const fast = $("input:radio{name=music]:checked").val();
+    const fast = $("input:radio[name=fast]:checked").val();
     const favoriteColor = $("#color").val();
 
-    if (animal === "Snake" && snakes === "no") {
-      $(".python").show();
-    } else if (animal === "Songbird" && music === "yes") {
-      $(".Csharp").show();
-    } else if (animal === "Hedgehog" && fast === "yes") {
-      $(".swift").show();
+    if (animal === 2 && snakes === "no") {
+      $("#python").show();
+    } else if (animal === 1 && music === "yes") {
+      $("#Csharp").show();
+    } else if (animal === 3 && fast === "yes") {
+      $("#swift").show();
     } else {
-      $(".javascript").show();
+      $("#javascript").show();
     }
+    $(body).css(background-color, favoriteColor);
   });
 });
