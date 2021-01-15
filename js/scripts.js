fa8073181d6969e4dc6ceca383df8ad1cc2b23a9
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-15 12:40:45 -0800

    Work on getting modal to work/debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 let modal = $("resultsModal");
 let btn = $("resultsButton");
+let span = $("close")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
